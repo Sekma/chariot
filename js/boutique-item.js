cc278527ed570d9e0f43213e53742b0f2a11e90c
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-11-14 23:52:20 +0100

    menu image display

---
+++ js/boutique-item.js
@@ -0,0 +1,15 @@
+$(document).ready(function (){
+    let searchParams = new URLSearchParams(window.location.search);
+    if(searchParams.has('id')){
+       $.getJSON('/js/boutique.json', function (data) {
+        let menuToDisplay = data[searchParams.get('id')];
+        $('#boutique-item h1').text(menuToDisplay.title);
+        let rootUl = $('#boutique-item ul');
+            $.each(menuToDisplay.image, function(index, image){
+            $(`<li><img src="img/categorie/${image}" /></li>`).appendTo(rootUl);
+        });
+        });
+    } else {
+        window.location.pathname = "boutique.html";
+    }
+});
