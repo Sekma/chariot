311142917e7d048dc5ca790c7a479b4e787434cf
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-11-18 13:19:43 +0100

    home image display

--- js/accueil.js
+++ js/accueil.js
@@ -5,11 +5,17 @@ $(document).ready(function () {
         });
         let rootSpecialites = $('.specialites-container ul');
 
-         $.each(monTableau, function(index, image){
-            $(`<li><img src="img/categorie/${image[1].articles.images[0]}" /></li>`).appendTo(rootSpecialites);
-            console.log(image[1].articles.images[0]);
-          });
+         $(`<li><img src="img/categorie/${monTableau[0][1].articles.images[5]}" /></li>`).appendTo(rootSpecialites);
+         $(`<li><img src="img/categorie/${monTableau[1][1].articles.images[12]}" /></li>`).appendTo(rootSpecialites);
+         $(`<li><img src="img/categorie/${monTableau[7][1].articles.image0}" /></li>`).appendTo(rootSpecialites);
 
+         let rootOffres = $('.offres-container ul');
+
+         $(`<li><img src="img/categorie/${monTableau[2][1].articles.image0}" /></li>`).appendTo(rootOffres);
+         $(`<li><img src="img/categorie/${monTableau[3][1].articles.images[5]}" /></li>`).appendTo(rootOffres);
+         $(`<li><img src="img/categorie/${monTableau[4][1].articles.images[1]}" /></li>`).appendTo(rootOffres);
+           
+            console.log(monTableau[0][1]);
         }
     );
 });
--- js/boutique.js
+++ js/boutique.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
     $.getJSON('js/boutique.json', function (data) {
-            console.log(data);
+        var monTableau = Object.keys(data).map(function(cle) {
+            return [Number(cle), data[cle]];
+        });
+        console.log(monTableau[0]);
+        let rootSpecialites = $('.boutique-main ul');
+
+         $.each(monTableau, function(index, image){
+            $(`<li><a href="boutique-item.html?id=${image[1].id}"><img src="img/categorie/${image[1].articles.image0}" /><span>${image[1].title}</span></a></li>`).appendTo(rootSpecialites);
+            
+          });
             
         }
     );
